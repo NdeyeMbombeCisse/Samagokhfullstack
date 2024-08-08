dd576c6641126cf3d5d7d70baf1366f31e03628d
SerigneFallouNiang <fallouniang776@gmail.com>
2024-08-08 23:42:05 +0000

    register of the auth

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    // Register API - POST (prenom, nom, email, password, etc.)
+    public function register(Request $request)
+    {
+        // Validation
+        $request->validate([
+            'commune_id' => 'nullable|exists:communes,id' ,// Validation pour vérifier que commune_id existe dans la table communes            'prenom' => $request->prenom,
+            'prenom' => 'required|string',
+            'nom' => 'required|string',
+            'date_naissance' => 'required|date',
+            'adresse' => 'required|string',
+            'lieu_naissance' => 'required|string',
+            'fonction' => 'nullable|in:eleve,bachelier,etudiant,diplome,mentor_certifie,profetionnel_reconvertit,retraite,chomeur',
+            'genre' => 'required|in:masculin,feminin',
+            'telephone' => 'required|string|unique:users',
+            'situation_matriminiale' => 'required|in:marie,divorce,celibataire,veuve',
+            'date_integration' => 'nullable|date',
+            'date_sortie' => 'nullable|date',
+            'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        // Handle file upload
+        $photoPath = null;
+        if ($request->hasFile('photo')) {
+            $photo = $request->file('photo');
+            $photoPath = $photo->store('photos', 'public'); // Store in public/photos directory
+        }
+
+        // User model to save user in database
+        User::create([
+            'commune_id' => $request->commune_id, // Ajouter l'ID de la commune
+            'nom' => $request->nom,
+            'date_naissance' => $request->date_naissance,
+            'adresse' => $request->adresse,
+            'lieu_naissance' => $request->lieu_naissance,
+            'fonction' => $request->fonction,
+            'genre' => $request->genre,
+            'telephone' => $request->telephone,
+            'situation_matriminiale' => $request->situation_matriminiale,
+            'date_integration' => $request->date_integration,
+            'date_sortie' => $request->date_sortie,
+            'photo' => $photoPath,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User registered successfully',
+            'data' => [
+                'photo' => $photoPath
+            ]
+        ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,26 @@ class User extends Authenticatable
     public function votes(){
         return $this->hasmany(Vote::class);
     }
+
+
+    //configuration du JWT de l'authentification
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+ 
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
