b12f59cf013c9b8832be1a8f4f19fef54daaf8db
BounAbdallah <bigmabo61345@gmail.com>
2024-08-20 12:22:35 +0000

    Ajout u seeder dans la base

--- database/migrations/2024_08_13_115413_update_cni_length_in_users_table.php
+++ database/migrations/2024_08_13_115413_update_cni_length_in_users_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('CNI', 13)->unique();
-        });
+        // Schema::table('users', function (Blueprint $table) {
+        //     $table->string('CNI', 13)->unique();
+        // });
     }
 
     /**
@@ -21,8 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        // Schema::table('users', function (Blueprint $table) {
+        //     //
+        // });
     }
 };
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,32 +15,39 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'nom' => "Serigne Fallou Niang",
-            'email' => "serignefallou@gmail.com",
-            'date_naissance' => "1990/01/01",
-            'adresse' => "Touba",
-            'lieu_naissance' => "Touba",
-            'genre' => "masculin",
-            'telephone' => "766149938",
-            'situation_matrimoniale' => "marie",  // Correction ici
-            'commune_id' => 2,
-            'email_verified_at' => now(),
-            'password' => Hash::make('password'),
-            'remember_token' => Str::random(10),
-        ],
-    [
-       'nom' => "Ndeye Mbombe Cisse",
-            'email' => "nmc@gmail.com",
-            'date_naissance' => "1990/01/01",
-            'adresse' => "Touba",
-            'lieu_naissance' => "Touba",
-            'genre' => "masculin",
-            'telephone' => "784055367",
-            'situation_matrimoniale' => "marie",  // Correction ici
-            'commune_id' => 2,
-            'email_verified_at' => now(),
-            'password' => Hash::make('password'),
-            'remember_token' => Str::random(10),
+            "commune_id"=> 1,
+            "prenom"=> "Bouna",
+            "nom"=> "Drame",
+            "CNI"=>"1793201800002",
+            "date_naissance"=> "1990-01-01",
+            "adresse"=> "123 Main St",
+            "lieu_naissance"=> "Cityville",
+            "fonction"=> "etudiant",
+            "genre"=> "masculin",
+            "telephone"=> "781860290",
+            "situation_matrimoniale"=> "celibataire",
+            "date_integration"=> "2024-01-01",
+            "date_sortie"=> "2024-12-31",
+            "photo"=> null,
+            "email"=> "bigmabo61345@gmail.com",
+            "password" => Hash::make('password'),
+            "remember_token" => Str::random(10),
+
+
+    //     ]
+    // [
+    //    'nom' => "Ndeye Mbombe Cisse",
+    //         'email' => "nmc@gmail.com",
+    //         'date_naissance' => "1990/01/01",
+    //         'adresse' => "Touba",
+    //         'lieu_naissance' => "Touba",
+    //         'genre' => "masculin",
+    //         'telephone' => "784055367",
+    //         'situation_matrimoniale' => "marie",  // Correction ici
+    //         'commune_id' => 2,
+    //         'email_verified_at' => now(),
+    //         'password' => Hash::make('password'),
+    //         'remember_token' => Str::random(10),
     ]);
     }
 }
